0978c1a8720197562c8c8f5b6e05310808b1753c
Chris Beach <chris@chrisbeach.co.uk>
2019-12-17 19:35:02 +0000

    Cats Effect migration

--- src/main/scala/com/brightercode/discoursefromtwitter/Runner.scala
+++ src/main/scala/com/brightercode/discoursefromtwitter/Runner.scala
@@ -1,10 +1,11 @@
 package com.brightercode.discoursefromtwitter
 
 import akka.actor.ActorSystem
-import cats.implicits._
-import cats.effect.{ExitCode, IO, IOApp}
-import com.brightercode.discourse.DiscourseForumApiClient.withDiscourseForum
-import com.brightercode.discourse.apidecorators.TopicApiAsync
+import cats.effect.concurrent.MVar
+import cats.effect.{ExitCode, IO, IOApp, Resource}
+import com.brightercode.discourse.DiscourseForumApiClient.{withDiscourseForum, discourseForumResource}
+import com.brightercode.discourse.apidecorators.AsyncTopicPoster
+import com.brightercode.discourse.model.{Topic, TopicTemplate}
 import com.brightercode.discourse.util.TypesafeConfigHelper._
 import com.brightercode.discoursefromtwitter.config.AppConfig
 import com.brightercode.discoursefromtwitter.discourse.TopicFactory.topicFrom
@@ -14,9 +15,6 @@ import com.danielasfregola.twitter4s.entities.Tweet
 import com.danielasfregola.twitter4s.http.clients.streaming.TwitterStream
 import com.typesafe.scalalogging.LazyLogging
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
 
 /**
@@ -30,27 +28,28 @@ object Runner extends IOApp with LazyLogging with AppConfig {
   private val twitter: TwitterStreamingClient = TwitterStreamingClient.withActorSystem(system)
 
   override def run(args: List[String]): IO[ExitCode] =
-    IO {
-      withDiscourseForum(discourseConfig, system) { forum =>
-        val topicCreatorAsync =  new TopicApiAsync(forum.topics)
-        Await.result(
-          withEveryStreamedTweet { tweet =>
-            topicFrom(tweet, categoryId)
-              .map(topicCreatorAsync.enqueue)
-              .getOrElse(logger.warn(s"Couldn't create topic from tweet: $tweet"))
-          },
-          atMost = 60 seconds
-        )
-        logger.info(s"Twitter stream initialised")
-        Await.result(system.whenTerminated, atMost = Duration.Inf)
-      }
-    }.as(ExitCode.Success)
+    discourseForumResource(discourseConfig, system).use { forum =>
+      val topicPoster = new AsyncTopicPoster(forum.topics)
+      for {
+        topicChannel <- MVar[IO].empty[TopicTemplate]
+        topicPostingFiber <- topicPoster.loop(topicChannel).start
+        tweetStream <- withEveryStreamedTweet { tweet =>
+          topicFrom(tweet, categoryId)
+            .map(topicChannel.put)
+            .getOrElse(logger.warn(s"Couldn't create topic from tweet: $tweet"))
+        }
+        _ <- IO(logger.info(s"Started stream: $tweetStream"))
+        _ <- topicPostingFiber.join
+      } yield ExitCode.Success
+    }
 
-  private def withEveryStreamedTweet(operation: Tweet => Any): Future[TwitterStream] = {
+  private def withEveryStreamedTweet(operation: Tweet => Any): IO[TwitterStream] = {
     logger.info(s"Establishing tweet stream, tracks=[${tracks.mkString(", ")}], follow=[${follow.mkString(", ")}]")
-    twitter.filterStatuses(tracks = tracks, follow = follow)({
-      case tweet: Tweet => operation(tweet)
-      case other => logger.trace(other.toString)
-    }, errorHandler = exitOnError)
+    IO.fromFuture { IO {
+      twitter.filterStatuses(tracks = tracks, follow = follow, stall_warnings = true)({
+        case tweet: Tweet => operation(tweet)
+        case other => logger.debug(other.toString)
+      }, errorHandler = exitOnError)
+    }}
   }
 }
